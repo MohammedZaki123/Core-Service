ed3c8fdd4e136a6a87e9427ef54b03a93d33fad5
Mohammed Mahmoud <mohammedzaki006@gmail.com>
2026-04-18 16:37:44 +0200

    Member and RBAC module with all of its functionalities implemented and tested

---
+++ src/app/rbac/errors.ts
@@ -0,0 +1,8 @@
+import {AppError} from "../../lib/error/AppError";
+
+export const CannotCreateOwnerUserError = new AppError('Not allowed to create another owner', 400);
+export const RoleNotFoundError = new AppError('Role not found', 404);
+
+export const MemberNotFoundError = new AppError('Member not found', 404);
+export const CannotDeleteOwnerError = new AppError('Cannot delete the restaurant owner', 400);
+export const IncorrectBranches = new AppError('some branches do not belong to this restaurant', 400)
