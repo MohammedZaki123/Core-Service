4494af2dc47169ca78cd702522d39bb4786b7629
Mohammed Mahmoud <mohammedzaki006@gmail.com>
2026-04-18 14:43:53 +0200

    DB migration Files Completed

--- src/migrations/20260303235422_create_restaurants_table.ts
+++ src/migrations/20260303235422_create_restaurants_table.ts
@@ -4,17 +4,23 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(`
     CREATE TABLE restaurants (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL UNIQUE,
-        status TEXT NOT NULL CHECK (status IN ('active', 'closed')),
+        id BIGSERIAL PRIMARY KEY,
+        owner_id BIGINT NOT NULL,
+        name TEXT NOT NULL,
+        status TEXT NOT NULL CHECK (status IN ('active', 'suspended', 'disabled', 'pending')) DEFAULT 'active',
         created_at TIMESTAMP NOT NULL,
         logo_url TEXT NOT NULL,
         primary_country TEXT NOT NULL,
-        updated_at TIMESTAMP NOT NULL
+        updated_at TIMESTAMP NOT NULL,
+        status_updated_at TIMESTAMP NOT NULL,
+        
+        constraint fk_restaurant_owner_id FOREIGN KEY (owner_id) REFERENCES users(id)
     );
-    CREATE INDEX idx_restaurants_name ON restaurants(name);
+    CREATE INDEX idx_restaurants_owner_id ON restaurants(owner_id);
     CREATE INDEX idx_restaurants_status ON restaurants(status);
-    `)
+    CREATE INDEX idx_restaurants_primary_country ON restaurants(primary_country);
+    CREATE INDEX idx_restaurants_created_at ON restaurants(created_at);
+    `);
 }
 
 
